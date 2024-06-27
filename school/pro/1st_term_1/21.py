e2f5aa289ed6efec1784b6192d08ec033e1f2a13
shiki-01 <0110shiki@gmail.com>
2024-06-27 19:34:41 +0900

    save

--- school/pro/1st_term_1/21.py
+++ school/pro/1st_term_1/21.py
@@ -1,230 +1,101 @@
-import os
-import platform
-import random
-import time
-import keyboard
+import pyxel
+import numpy as np
 
-def clear_terminal():
-    if platform.system() == "Windows":
-        os.system('cls')
-    else:
-        os.system('clear')
+class MazeGenerator:
+    def __init__(self, width, height, difficulty):
+        self.width = width
+        self.height = height
+        self.difficulty = difficulty
+        self.maze = np.ones((width, height), dtype=np.int8)
 
-COLOR_MAP = {
-    'R': '\033[91m●\033[0m',
-    'G': '\033[92m●\033[0m',
-    'B': '\033[94m●\033[0m',
-    'Y': '\033[93m●\033[0m',
-    None: ' '
-}
+    def generate_maze(self, x=0, y=0):
+        directions = [(0, -2), (2, 0), (0, 2), (-2, 0)]
+        np.random.shuffle(directions)
+        for dx, dy in directions:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < self.width and 0 <= ny < self.height and self.maze[nx, ny]:
+                self.maze[nx - dx // 2, ny - dy // 2] = 0
+                self.maze[nx, ny] = 0
+                self.generate_maze(nx, ny)
 
-ORIENTATIONS = [
-    'up',
-    'right',
-    'down',
-    'left'
-]
+        self.adjust_maze_complexity()
 
-BOARD_WIDTH = 6
-BOARD_HEIGHT = 12
+    def adjust_maze_complexity(self):
+        if self.difficulty == 0:
+            for _ in range(int(self.width * self.height * 0.07)):
+                x, y = np.random.randint(1, self.width - 1, size=2)
+                if self.maze[x, y] == 1:
+                    self.maze[x, y] = 0
+        elif self.difficulty == 2:
+            for _ in range(int(self.width * self.height * 0.15)):
+                x, y = np.random.randint(1, self.width - 1, size=2)
+                if self.maze[x, y] == 0:
+                    self.maze[x, y] = 1
 
-class PuyoPuyo:
+class App:
+    def __init__(self, difficulty):
+        self.difficulty = difficulty
+        self.pixel_size = 5 + 1 * difficulty
+        self.player_x, self.player_y = 0, 0
+        if difficulty == 0:
+            width, height = 30, 20
+            self.goal_x, self.goal_y = 145, 95
+        elif difficulty == 1:
+            width, height = 40, 30
+            self.goal_x, self.goal_y = 195, 145
+        else:
+            width, height = 50, 40
+            self.goal_x, self.goal_y = 245, 195
+
+        generator = MazeGenerator(width, height, difficulty)
+        generator.generate_maze()
+        self.maze = generator.maze
+        self.maze[0][0] = 0
+        self.maze[-2][-1] = 0
+        self.maze[-1][-1] = 0
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        move_distance = self.pixel_size
+        if pyxel.btnp(pyxel.KEY_LEFT) and self.player_x >= move_distance and self.maze[(self.player_x - move_distance) // move_distance, self.player_y // move_distance] == 0:
+            self.player_x -= move_distance
+        if pyxel.btnp(pyxel.KEY_RIGHT) and self.player_x <= pyxel.width - move_distance and self.maze[(self.player_x + move_distance) // move_distance, self.player_y // move_distance] == 0:
+            self.player_x += move_distance
+        if pyxel.btnp(pyxel.KEY_UP) and self.player_y >= move_distance and self.maze[self.player_x // move_distance, (self.player_y - move_distance) // move_distance] == 0:
+            self.player_y -= move_distance
+        if pyxel.btnp(pyxel.KEY_DOWN) and self.player_y <= pyxel.height - move_distance and self.maze[self.player_x // move_distance, (self.player_y + move_distance) // move_distance] == 0:
+            self.player_y += move_distance
+
+    def draw(self):
+        pyxel.cls(0)
+        pixel_size = self.pixel_size
+        for y in range(self.maze.shape[1]):
+            for x in range(self.maze.shape[0]):
+                if self.maze[x, y] == 1:
+                    pyxel.rect(x * pixel_size, y * pixel_size, pixel_size, pixel_size, 8)
+        pyxel.rect(self.player_x, self.player_y, pixel_size, pixel_size, 10)
+        pyxel.rect(self.goal_x, self.goal_y, pixel_size, pixel_size, 14)
+
+class Start:
     def __init__(self):
-        self.board = [[None] * BOARD_WIDTH for _ in range(BOARD_HEIGHT)]
-        self.fixed_puyos = [[None] * BOARD_WIDTH for _ in range(BOARD_HEIGHT)]
-        self.current_puyo = None
-        self.next_puyo()
+        pyxel.init(150, 100, title='Maze')
+        self.difficulty = 0
+        pyxel.run(self.update_difficulty, self.draw_difficulty)
 
-    def next_puyo(self):
-        self.current_puyo = {
-            'position': [BOARD_HEIGHT - 1, BOARD_WIDTH // 2 - 1],
-            'orientation': 'up',
-            'colors': [random.choice(['R', 'G', 'B', 'Y']), random.choice(['R', 'G', 'B', 'Y'])]
-        }
-        self.update_board()
+    def update_difficulty(self):
+        if pyxel.btnp(pyxel.KEY_UP) and self.difficulty > 0:
+            self.difficulty -= 1
+        if pyxel.btnp(pyxel.KEY_DOWN) and self.difficulty < 2:
+            self.difficulty += 1
+        if pyxel.btnp(pyxel.KEY_SPACE):
+            App(self.difficulty)
 
-    def update_board(self):
-        self.clear_board()
-        row, column = self.current_puyo['position']
-        if 0 <= row < BOARD_HEIGHT and 0 <= column < BOARD_WIDTH:
-            self.board[row][column] = COLOR_MAP[self.current_puyo['colors'][0]]
-            if self.current_puyo['orientation'] == 'up':
-                self.board[row-1][column] = COLOR_MAP[self.current_puyo['colors'][1]]
-            elif self.current_puyo['orientation'] == 'down':
-                self.board[row-1][column] = COLOR_MAP[self.current_puyo['colors'][1]]
-            elif self.current_puyo['orientation'] == 'left':
-                self.board[row][column-1] = COLOR_MAP[self.current_puyo['colors'][1]]
-            elif self.current_puyo['orientation'] == 'right':
-                self.board[row][column+1] = COLOR_MAP[self.current_puyo['colors'][1]]
-        self.merge_boards()
+    def draw_difficulty(self):
+        pyxel.cls(0)
+        pyxel.text(50, 40, "Select Difficulty:", 7)
+        difficulties = ["Easy", "Normal", "Hard"]
+        for i, difficulty in enumerate(difficulties):
+            color = pyxel.COLOR_LIGHT_BLUE if i == self.difficulty else pyxel.COLOR_WHITE
+            pyxel.text(50, 50 + i * 10, difficulty, color)
 
-    def merge_boards(self):
-        for i in range(BOARD_HEIGHT):
-            for j in range(BOARD_WIDTH):
-                if self.fixed_puyos[i][j] is not None:
-                    self.board[i][j] = self.fixed_puyos[i][j]
-
-    def clear_board(self):
-        for row in range(BOARD_HEIGHT):
-            for column in range(BOARD_WIDTH):
-                if self.board[row][column] in COLOR_MAP.values() and (row, column) != tuple(self.current_puyo['position']):
-                    self.board[row][column] = None
-
-    def drop(self):
-        old_row, old_column = self.current_puyo['position']
-        if 0 <= old_row < BOARD_HEIGHT and 0 <= old_column < BOARD_WIDTH:
-            if self.current_puyo['orientation'] == 'up' or self.current_puyo['orientation'] == 'down':
-                if old_row > 1 and self.board[old_row-2][old_column] is None:
-                    self.current_puyo['position'][0] -= 1
-                    self.update_board()
-                    return True
-            elif self.current_puyo['orientation'] == 'left':
-                if old_row > 0 and self.board[old_row-1][old_column] is None and self.board[old_row-1][old_column-1] is None:
-                    self.current_puyo['position'][0] -= 1
-                    self.update_board()
-                    return True
-            elif self.current_puyo['orientation'] == 'right':
-                if old_row > 0 and self.board[old_row-1][old_column] is None and self.board[old_row-1][old_column+1] is None:
-                    self.current_puyo['position'][0] -= 1
-                    self.update_board()
-                    return True
-        return self.handle_drop_failure()
-
-    def fix_current_puyo(self):
-        row, column = self.current_puyo['position']
-        self.fixed_puyos[row][column] = COLOR_MAP[self.current_puyo['colors'][0]]
-        if self.current_puyo['orientation'] == 'up' or self.current_puyo['orientation'] == 'down':
-            self.fixed_puyos[row-1][column] = COLOR_MAP[self.current_puyo['colors'][1]]
-        elif self.current_puyo['orientation'] == 'left':
-            self.fixed_puyos[row][column-1] = COLOR_MAP[self.current_puyo['colors'][1]]
-        elif self.current_puyo['orientation'] == 'right':
-            self.fixed_puyos[row][column+1] = COLOR_MAP[self.current_puyo['colors'][1]]
-        self.drop_all_puyos()
-
-    def drop_all_puyos(self):
-        dropped = True
-        while dropped:
-            dropped = False
-            for row in range(1, BOARD_HEIGHT):
-                for column in range(BOARD_WIDTH):
-                    if self.fixed_puyos[row][column] is not None and self.fixed_puyos[row-1][column] is None:
-                        self.fixed_puyos[row-1][column] = self.fixed_puyos[row][column]
-                        self.fixed_puyos[row][column] = None
-                        dropped = True
-                        self.update_board()
-                        self.print_board()
-                        time.sleep(0.1)
-        self.remove_matched()
-        self.update_board()
-
-    def remove_matched(self):
-        visited = [[False]*BOARD_WIDTH for _ in range(BOARD_HEIGHT)]
-        for row in range(BOARD_HEIGHT):
-            for column in range(BOARD_WIDTH):
-                if self.fixed_puyos[row][column] is not None and not visited[row][column]:
-                    stack = [(row, column)]
-                    puyo_group = []
-                    while stack:
-                        current_row, current_column = stack.pop()
-                        if not visited[current_row][current_column] and self.fixed_puyos[current_row][current_column] == self.fixed_puyos[row][column]:
-                            visited[current_row][current_column] = True
-                            puyo_group.append((current_row, current_column))
-                            for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                                next_row, next_column = current_row + dr, current_column + dc
-                                if 0 <= next_row < BOARD_HEIGHT and 0 <= next_column < BOARD_WIDTH:
-                                    stack.append((next_row, next_column))
-                    if len(puyo_group) >= 4:
-                        for puyo_row, puyo_column in puyo_group:
-                            self.fixed_puyos[puyo_row][puyo_column] = None
-                        self.drop_all_puyos()
-
-    def handle_drop_failure(self):
-        self.fix_current_puyo()
-        self.remove_matched()
-        if self.current_puyo['position'][0] >= BOARD_HEIGHT - 1:
-            print("Game Over")
-            return "GameOver"
-        else:
-            self.next_puyo()
-            return False
-
-    def move_left(self):
-        old_row, old_column = self.current_puyo['position']
-        if old_column > 0 and self.board[old_row][old_column-1] is None:
-            self.current_puyo['position'][1] -= 1
-            self.update_board()
-
-    def move_right(self):
-        old_row, old_column = self.current_puyo['position']
-        if old_column < BOARD_WIDTH - 1 and self.board[old_row][old_column+1] is None:
-            self.current_puyo['position'][1] += 1
-            self.update_board()
-
-    def rotate_puyo(self, direction):
-        old_row, old_column = self.current_puyo['position']
-        old_orientation = self.current_puyo['orientation']
-        old_index = ORIENTATIONS.index(old_orientation)
-
-        if direction == 'right':
-            new_index = (old_index + 1) % 4
-        else:
-            new_index = (old_index - 1) % 4
-
-        new_orientation = ORIENTATIONS[new_index]
-
-        can_rotate = self.check_rotation_feasibility(new_orientation, old_row, old_column)
-        if can_rotate:
-            self.current_puyo['orientation'] = new_orientation
-            self.update_board()
-
-    def check_rotation_feasibility(self, new_orientation, old_row, old_column):
-        if new_orientation == 'left':
-            return (0 <= old_column - 1 < BOARD_WIDTH) and (self.board[old_row][old_column - 1] is None)
-        elif new_orientation == 'right':
-            return (0 <= old_column + 1 < BOARD_WIDTH) and (self.board[old_row][old_column + 1] is None)
-        elif new_orientation == 'up':
-            return (0 <= old_row + 1 < BOARD_HEIGHT) and (self.board[old_row + 1][old_column] is None)
-        elif new_orientation == 'down':
-            return (0 <= old_row - 1 < BOARD_HEIGHT) and (self.board[old_row - 1][old_column] is None)
-
-
-    def print_board(self):
-        clear_terminal()
-        top_border = '┌─' + '─'*(BOARD_WIDTH * 2 - 1) + '─┐'
-        bottom_border = '└─' + '─'*(BOARD_WIDTH * 2 - 1) + '─┘'
-        print(top_border)
-        for row in self.board[::-1]:
-            row_content = [' ' if cell is None else cell for cell in row]
-            row_str = '│ ' + ' '.join(row_content) + ' │'
-            print(row_str)
-        print(bottom_border)
-clear_terminal()
-game = PuyoPuyo()
-
-while True:
-    game.print_board()
-    while True:
-        result = game.drop()
-        if result == "GameOver":
-            print("Game Over")
-            break
-        if keyboard.is_pressed('left'):
-            game.move_left()
-        elif keyboard.is_pressed('right'):
-            game.move_right()
-        elif keyboard.is_pressed('a'):
-            game.rotate_puyo('left')
-        elif keyboard.is_pressed('d'):
-            game.rotate_puyo('right')
-        elif keyboard.is_pressed('q'):
-            result = "Quit"
-            break
-
-        game.print_board()
-        if keyboard.is_pressed('down'):
-            time.sleep(0.1)
-        else:
-            time.sleep(0.5)
-
-    if result == "GameOver" or result == "Quit":
-        break
+Start()
